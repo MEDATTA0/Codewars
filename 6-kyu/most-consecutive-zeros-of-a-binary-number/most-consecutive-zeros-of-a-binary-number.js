af6d54d1939d600850f42909141027110c9589ba
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-09 09:43:56 +0200

    Update solution - CodeHub

--- 6-kyu/most-consecutive-zeros-of-a-binary-number/most-consecutive-zeros-of-a-binary-number.js
+++ 6-kyu/most-consecutive-zeros-of-a-binary-number/most-consecutive-zeros-of-a-binary-number.js
@@ -3,6 +3,13 @@ function maxConsecZeros(n){
   const toReadable = {↔}
   const zeros = n.toString(2).match(/(0)\1*/g)
   console.log(zeros)
+  let max = 0;
   
+    if(zero.length < max){
+      return false
+    }
+    max = zero.length
+    max
+  })
   return max;
 }
