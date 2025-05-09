3d94c3e7dd1fccf62d661046dcc8a4b6d0e084c7
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-09 10:28:17 +0200

    Update solution - CodeHub

--- 6-kyu/last-non-zero-digit-of-factorial/last-non-zero-digit-of-factorial.js
+++ 6-kyu/last-non-zero-digit-of-factorial/last-non-zero-digit-of-factorial.js
@@ -3,10 +3,11 @@ function lastDigit(n) {
   const numbers = []
   for(let i = n; i > 2; i--){
     let filteredI = i;
-    while(!(n % 5) && !(n % 2)){
-      filteredI = filteredI % 10
+    while(n % 5 === 0 && n % 2 === 0 && n !== 0){
       
+      filteredI = filteredI % 10
     }
+    if(filteredI === 0) filteredI = 1
     numbers.push(filteredI)
   }
   console.log(numbers)
