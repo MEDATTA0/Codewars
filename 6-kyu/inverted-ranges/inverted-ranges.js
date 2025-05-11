a3ba65a32a55bd6b0e1d30d7ebca42e8aa49709e
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-11 10:13:24 +0200

    Update solution - CodeHub

--- 6-kyu/inverted-ranges/inverted-ranges.js
+++ 6-kyu/inverted-ranges/inverted-ranges.js
@@ -9,14 +9,20 @@ function invertedRanges(ranges) {
     for(let j = range[0]; j <= range[1]; j++) includedNumbers.push(j)
   }
   
+  // Check if the first included number is greater than 0
+  // If so, we need to start the first excluded range from 0
   if(includedNumbers[0] !== 0){
     excludedNumbers.push([0, includedNumbers[0] - 1])
   }
   
+  // Loop through the included numbers to find any gaps
   for(let i = 0; i < includedNumbers.length; i++){
-    const nextNumber = includedNumbers[i + 1];
-    const nextInteger = includedNumbers[i] + 1
-    if(nextNumber !== nextInteger && nextInteger <= 100){
+    const nextNumber = includedNumbers[i + 1]; // The next number in the list
+    const nextExpected = includedNumbers[i] + 1 // What the next number *should* be if no gap exists
+​
+    
+    // If there's a gap between current and next number
+    if(nextNumber !== nextExpected && nextExpected <= 100){
         excludedNumbers.push([includedNumbers[i] + 1, includedNumbers[i+1] - 1 || 100])
     }
   }
