dcfad6475317d26aa48487e634bc1b2fd8d72a5e
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-11 09:17:53 +0200

    Update solution - CodeHub

--- 6-kyu/inverted-ranges/inverted-ranges.js
+++ 6-kyu/inverted-ranges/inverted-ranges.js
@@ -2,9 +2,10 @@
 function invertedRanges(ranges) {
   if(ranges.length === 0) return [[0, 100]]
   if(ranges[0].join("") === "0100") return []
-  for(let i = 0; i <= ranges.length; i++){
-    const previousHighestBound = ranges[i];
-    
-    for(let j )
-  }
+  
+//   for(let i = 0; i < ranges.length; i++){
+//     const previousHigherBound = ranges[i][1];
+//     const nextLowerBound = ranges[i++][0];
+//     console.log(previousHigherBound, nextLowerBound)
+//   }
 }
