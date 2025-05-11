ada910f5732e7150750c753c744bbbba5b5bd477
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-11 09:28:34 +0200

    Update solution - CodeHub

--- 6-kyu/inverted-ranges/inverted-ranges.js
+++ 6-kyu/inverted-ranges/inverted-ranges.js
@@ -2,10 +2,14 @@
 function invertedRanges(ranges) {
   if(ranges.length === 0) return [[0, 100]]
   if(ranges[0].join("") === "0100") return []
+//   const allNumbers = [];
+  const includedNumbers = []
   
-//   for(let i = 0; i < ranges.length; i++){
-//     const previousHigherBound = ranges[i][1];
-//     const nextLowerBound = ranges[i++][0];
-//     console.log(previousHigherBound, nextLowerBound)
-//   }
+//   for(let i = 0; i <= 100; i++) allNumbers.push(i);
+  for(let i = 0; i < ranges.length; i++){
+    const range = ranges[i];
+    for(let j = range[0]; j <= range[1]; j++) includedNumbers.push(j)
+  }
+  
+  console.log(includedNumbers)
 }
