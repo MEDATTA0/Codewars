45df40382b34d96ec905c2a376d65ed61c05596d
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-11 09:09:50 +0200

    Update solution - CodeHub

--- 6-kyu/inverted-ranges/inverted-ranges.js
+++ 6-kyu/inverted-ranges/inverted-ranges.js
@@ -1,6 +1,6 @@
  
 function invertedRanges(ranges) {
   if(ranges.length === 0) return [[0, 100]]
+  if(ranges[0].join("") === "0100") return []
   
-  else if(ranges[0].join("") === "0100") return []
 }
