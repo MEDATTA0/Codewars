72c5794525f60de670dabeb97085f026a51256b2
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-12 10:28:48 +0200

    Update solution - CodeHub

--- 5-kyu/mean-square-error/mean-square-error.py
+++ 5-kyu/mean-square-error/mean-square-error.py
@@ -1,7 +1,5 @@
- 
 def solution(array_a, array_b):
     squares = []
     for i in range(len(array_a)):
-        square.append(abs(array_a[i] - array_b[i]) ** 2)
-        
-    return 0
+        squares.append(abs(array_a[i] - array_b[i]) ** 2)
+    return sum(squares) / len(squares)
