7dae45620bb17ebc5fa7f8648efd5ebf3715ce3c
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-12 10:38:41 +0200

    Update solution - CodeHub

--- 6-kyu/arrh-grabscrab/arrh-grabscrab.js
+++ 6-kyu/arrh-grabscrab/arrh-grabscrab.js
@@ -1,6 +1,10 @@
+ 
 function grabscrab(anagram, dictionary) {
-  
+  const matches = [];
   for(const word of dictionary){
-    
+    let matched = false
+    for(let i = 0; i < anagram.length; i++){
+      if()
+    }
   }
 }
