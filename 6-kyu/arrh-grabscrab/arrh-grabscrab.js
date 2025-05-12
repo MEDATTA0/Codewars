51549da2ed25b1c554d7e6c286e8b47bbdd1e2e1
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-12 11:35:06 +0200

    Update solution - CodeHub

--- 6-kyu/arrh-grabscrab/arrh-grabscrab.js
+++ 6-kyu/arrh-grabscrab/arrh-grabscrab.js
@@ -2,16 +2,18 @@
 function grabscrab(anagram, dictionary) {
   const matches = [];
   const anagramObject = {};
+//  convert anagram to {letter: frequency}
   for(let i = 0; i < anagram.length; i++){
     if(anagramObject[anagram[i]]) anagramObject[anagram[i]]++;
     else {
       anagramObject[anagram[i]] = 1
     }
   }
-  console.log(anagramObject)
+//   convert word to {letter: frequency}
   for(const word of dictionary){
+    
     if(word.length !== anagram.length)
-      break;
+      continue;
     const wordObject = {}
     word.split("").forEach((letter)=>{
       if(wordObject[letter]) wordObject[letter]++;
@@ -19,7 +21,7 @@ function grabscrab(anagram, dictionary) {
       wordObject[letter] = 1
       }
     })
-    
+​
     let matched = false
     for(const letter in anagramObject){
       if(wordObject[letter] !== anagramObject[letter]){
