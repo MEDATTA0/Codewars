cb94db81ae923d3ed435c4a564ad18e538bab93a
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-12 10:48:14 +0200

    Update solution - CodeHub

--- 6-kyu/arrh-grabscrab/arrh-grabscrab.js
+++ 6-kyu/arrh-grabscrab/arrh-grabscrab.js
@@ -2,14 +2,10 @@
 function grabscrab(anagram, dictionary) {
   const matches = [];
   for(const word of dictionary){
-    let notMatched = false
-    for(let i = 0; i < anagram.length; i++){
-      if(!word.includes(anagram[i])){
-        notMatched = true
-        break;
-      }
-    }
-    if(!notMatched) matches.push(word)
+    let notMatched = true
+    const letters = anagram.split("")
+    if(!letter.some((letter) => !word.includes(letter)))
+      matches.push(word)
     console.log(matches)
   }
   return matches;
