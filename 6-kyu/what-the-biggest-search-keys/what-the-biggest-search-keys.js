39cc3e0324cae5e746f53c046abea2ca0fe24eaf
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-13 12:10:13 +0200

    Update solution - CodeHub

--- 6-kyu/what-the-biggest-search-keys/what-the-biggest-search-keys.js
+++ 6-kyu/what-the-biggest-search-keys/what-the-biggest-search-keys.js
@@ -1,10 +1,13 @@
  
 function theBiggestSearchKeys(...keywords) {
-  const sortedKeywords = keywords.sort().reverse()
-  let output = ""
-  sortedKeywords.some((keyword) => {
-    if(keyword.length === sortedKeywords[0].length)
-      output += `${keyword}`
-  }).sort().join(", ")
+  const lengths = [];
   
+  const sortedKeywords = keywords.sort().reverse()
+  let output = [];
+  sortedKeywords.some((keyword) => {
+    if(keyword.length === sortedKeywords[0].length){
+      output.push(`\'${keyword}\'`)
+    }
+  })
+  return output.length ? output.reverse().join(", ") : "\'\'"
 }
