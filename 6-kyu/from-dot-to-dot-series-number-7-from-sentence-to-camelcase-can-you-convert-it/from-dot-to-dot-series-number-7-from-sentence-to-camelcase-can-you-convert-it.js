587e10f92da3993249b953e5f3ef1cf4f67c5e62
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-14 11:13:27 +0200

    Update solution - CodeHub

--- 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
+++ 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
@@ -1,9 +1,18 @@
  
 function toCamelCase(s,n){
-  
+  const words = s.split(" ")
   switch (n) {
       case 1:
-        
+        return words.map((word, index) => {
+          if(index === 0) return word;
+          return word[0].toUpperCase() + word.slice(1)
+        }).join("")
+      case 2:
+        return words.map((word, index) => {
+          if(index === word.length - 1) return word;
+          return word.slice(0, word.legth - 1) + word[word.length - 1].toUpperCase()
+        }).join("")
+      c
   }
 }
 ​
