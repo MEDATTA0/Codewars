e42002b9a2c1f6cc730f50a52a010bba69fe7bb3
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-14 11:19:10 +0200

    Update solution - CodeHub

--- 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
+++ 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
@@ -1,18 +1,29 @@
  
 function toCamelCase(s,n){
   const words = s.split(" ")
-  
+  let result;
   switch (n) {
       case 1:
-        return words.map((word, index) => {
+        result = words.map((word, index) => {
           if(index === 0) return word;
           return word[0].toUpperCase() + word.slice(1)
         }).join("")
+        break;
       case 2:
-        return words.map((word, index) => {
-          if(index === word.length - 1) return word;
-          return word.slice(0, word.length - 1) + word[word.length - 1].toUpperCase()
+        result = words.map((word, index) => {
+          if(index === words.length - 1) return word;
+          return word.slice(0, word.length - 1) + word.at(-1).toUpperCase()
         }).join("")
+        break;
+      case 3:
+        result = words.map((word, index) => {
+          if(index === words.length - 1) return word;
+          return word.slice(0, word.length - 1) + word.at(-1).toUpperCase()
+          
+        }).join("")
+        break;
   }
+  console.log(result)
+  return result
 }
 ​
