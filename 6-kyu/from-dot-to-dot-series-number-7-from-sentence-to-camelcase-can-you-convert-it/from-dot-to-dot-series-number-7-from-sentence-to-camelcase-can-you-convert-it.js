a955792f4ab369f97f7a03c5b7f5a1ca4dc9cba1
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-14 11:44:04 +0200

    Update solution - CodeHub

--- 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
+++ 6-kyu/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it/from-dot-to-dot-series-number-7-from-sentence-to-camelcase-can-you-convert-it.js
@@ -1,29 +1,23 @@
- 
 function toCamelCase(s,n){
   const words = s.split(" ")
   let result;
   switch (n) {
       case 1:
-        result = words.map((word, index) => {
-          if(index === 0) return word;
-          return word[0].toUpperCase() + word.slice(1)
+        return words.map((word, index) => {
+          if(index === 0) return word.toLowerCase();
+          return word[0].toUpperCase() + word.slice(1).toLowerCase();
         }).join("")
-        break;
       case 2:
-        result = words.map((word, index) => {
-          if(index === words.length - 1) return word;
-          return word.slice(0, word.length - 1) + word.at(-1).toUpperCase()
+        return words.map((word, index) => {
+          if(index === words.length - 1) return word.toLowerCase();
+          return word.slice(0, word.length - 1).toLowerCase() + word.at(-1).toUpperCase()
         }).join("")
-        break;
       case 3:
-        result = words.map((word, index) => {
-          if(index === words.length - 1) return word;
-          return word.slice(0, word.length - 1) + word.at(-1).toUpperCase()
-          
+        return words.map((word, index) => {
+          if(index === 0) return word.slice(0, word.length - 1).toLowerCase() + word.at(-1).toUpperCase()
+          else if (index === words.length - 1) return word[0].toUpperCase() + word.slice(1).toLowerCase();
+          return word[0].toUpperCase() + word.slice(1, word.length - 1).toLowerCase() + word.at(-1).toUpperCase()
         }).join("")
-        break;
   }
-  console.log(result)
-  return result
 }
 ​
