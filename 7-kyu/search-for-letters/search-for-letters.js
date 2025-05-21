91ccb4f458469d83fdd459969267259cc6f1ff98
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-21 10:51:28 +0200

    Update solution - CodeHub

--- 7-kyu/search-for-letters/search-for-letters.js
+++ 7-kyu/search-for-letters/search-for-letters.js
@@ -1,4 +1,9 @@
- 
 function change(string){
-  
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const includedLetters = Array(26).fill(0);
+  string.toLowerCase().split("").forEach((value)=>{
+    const letterIndex = letters.indexOf(value)
+    if(letterIndex !== -1 && includedLetters[letterIndex] === 0) includedLetters.splice(letterIndex, 1, 1)
+  })
+  return includedLetters.join("");
 }
