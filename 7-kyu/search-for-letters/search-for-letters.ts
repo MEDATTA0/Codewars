e2d351d00a21b4d62f097b5001ba63e7f2bb6c08
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-30 14:37:30 +0200

    Update solution - CodeHub

--- 7-kyu/search-for-letters/search-for-letters.ts
+++ 7-kyu/search-for-letters/search-for-letters.ts
@@ -1,4 +1,3 @@
- 
 export function change(string: string): string{
-  
+  return "abcdefghijklmnopqrstuvwxyz".split("").map((el: string): number=>{return +string.toLowerCase().includes(el)}).join("")
 }
