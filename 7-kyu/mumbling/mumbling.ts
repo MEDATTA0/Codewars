e042a7aa00f6c521ed4837a527a58a5d841c2a47
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-05-30 14:04:59 +0200

    Update solution - CodeHub

--- 7-kyu/mumbling/mumbling.ts
+++ 7-kyu/mumbling/mumbling.ts
@@ -1,5 +1,3 @@
- 
 export function accum(s: string): string {
-  
-  
+  return s.split("").map((letter: string, index: number) => {return `${letter.toUpperCase()}`.padEnd(index + 1, letter.toLowerCase())}).join("-")
 }
