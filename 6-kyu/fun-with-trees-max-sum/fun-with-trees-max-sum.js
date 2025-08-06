15c0604bdb6519978eb96f636c2e6a33b6a304f8
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-08-06 11:39:23 +0200

    Update solution - CodeHub

--- 6-kyu/fun-with-trees-max-sum/fun-with-trees-max-sum.js
+++ 6-kyu/fun-with-trees-max-sum/fun-with-trees-max-sum.js
@@ -1,11 +1,9 @@
  
 function maxSum(root) {
-  console.log(root)
   const visited = new Set();
   const queue = [root];
   visited.add(JSON.stringify(root));
+  if(root === null) return 0;
+  console.log( root.value + maxSum(root.left) + maxSum(root.right))  
   
-  for(const left, right of )
-  
-  return 0; // TODO: implementation
 }
