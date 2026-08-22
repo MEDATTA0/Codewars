3f9076b72d4aaee19e33cdbe3444895b6dee3eb4
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2026-08-22 16:22:37 -0400

    Update solution - CodeHub

--- 6-kyu/persistent-bugger/persistent-bugger.go
+++ 6-kyu/persistent-bugger/persistent-bugger.go
@@ -1,22 +1,16 @@
 package kata
 ​
-import (
-  "strconv"
-  "strings"
-)
-​
 func Persistence(n int) int {
   count := 0
   for n > 9 {
-    count++
-    result := 1
-    for v := range strings.SplitSeq(strconv.Itoa(n), "") {
-      num, _ := strconv.Atoi(v)
-      result *= num
+    temp := 1
+    for n > 0 {
+      temp *= n % 10
+      n /= 10
     }
-    n = result
+    n = temp
+    count++
   }
 ​
   return count
 }
-​
